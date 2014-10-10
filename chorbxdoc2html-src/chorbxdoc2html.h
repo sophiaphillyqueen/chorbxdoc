d6104a7ded929291b266d0fe42df93915c0bd3a1
Sophia Elizabeth Shapira <Sophia.Shapira@gmail.com>
2014-10-09 23:10:47 -0400

    Now there's even an install target!

---
+++ chorbxdoc2html-src/chorbxdoc2html.h
@@ -0,0 +1,26 @@
+// chorbxdoc2html - Converts a -chorbxdoc- document to HTML.
+// Copyright (C) 2014  Sophia Elizabeth Shapira
+//
+// This program is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with this program.  If not, see <http://www.gnu.org/licenses/>.
+//
+// ########################
+
+
+#ifndef LOCAL_INC__CHORBXDOC2HTML__H
+#define LOCAL_INC__CHORBXDOC2HTML__H
+
+
+
+#endif
+
--- chorbxdoc2html-src/main.c
+++ chorbxdoc2html-src/main.c
@@ -1,4 +1,6 @@
 
+#include "chorbxdoc2html.h"
+
 int main ( int argc, char **argv, char **env )
 {
   return 0;
